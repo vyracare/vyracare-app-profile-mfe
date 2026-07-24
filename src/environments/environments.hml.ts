51ecc8c8b10ef99938883cf1b56af06e0a8123a9
Lênin Müller Sampaio <lenin_muller@uol.com.br>
2026-07-24 13:12:05 -0300

    corrige urls de ambientes do app profile

--- src/environments/environments.hml.ts
+++ src/environments/environments.hml.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ef39m2gyya.execute-api.us-east-1.amazonaws.com/api/auth'
+  apiUrl: 'https://jkvfvgsw4l.execute-api.us-east-1.amazonaws.com/api/auth'
 };
